689d2b71f473bd161206fa715d981fa462c903b2
AngelaZou2000 <az292@cornell.edu>
2019-11-23 15:32:24 -0500

    parse string try

--- MultiSerial.ino
+++ MultiSerial.ino
@@ -98,21 +98,21 @@ void setup() {
 void loop() {    
   String pos_str = Serial1.readString();
   // Parse the string into angles
-  idx_begin = 0;
-  idx_end = length(pos_str);
-  idx_comma = pos_str.indexOf(',', idx_begin);
-  angle6 = pos_str[idx_begin : idx_comma];
-  idx_begin = idx_comma+1;
-  idx_comma = pos_str.indexOf(',', idx_begin);
-  angle5 = pos_str[idx_begin : idx_comma];
-  idx_begin = idx_comma+1;
-  idx_comma = pos_str.indexOf(',', idx_begin);
-  angle4 = pos_str[idx_begin : idx_comma];
-  idx_begin = idx_comma+1;
-  idx_comma = pos_str.indexOf(',', idx_begin);
-  angle3 = pos_str[idx_begin : idx_comma];
-  idx_begin = idx_comma+1;
-  angle2 = pos_str[idx_begin : idx_end];
+  int idx_begin = 0;
+  int idx_end = length(pos_str);
+  int idx_comma = pos_str.indexOf(',', idx_begin);
+  float angle6 = pos_str[idx_begin : idx_comma];
+  int idx_begin = idx_comma+1;
+  int idx_comma = pos_str.indexOf(',', idx_begin);
+  float angle5 = pos_str[idx_begin : idx_comma];
+  int idx_begin = idx_comma+1;
+  int idx_comma = pos_str.indexOf(',', idx_begin);
+  float angle4 = pos_str[idx_begin : idx_comma];
+  int idx_begin = idx_comma+1;
+  int idx_comma = pos_str.indexOf(',', idx_begin);
+  float angle3 = pos_str[idx_begin : idx_comma];
+  int idx_begin = idx_comma+1;
+  float angle2 = pos_str[idx_begin : idx_end];
   // end parse
   Serial.println(pos_str);
   arm.moveServos(5, 1000, 2, J2_deg_to_pos(angle2), 3, J3_deg_to_pos(angle3), 4, J4_deg_to_pos(angle4), 5, J5_deg_to_pos(angle5), 6, J6_deg_to_pos(angle6));
