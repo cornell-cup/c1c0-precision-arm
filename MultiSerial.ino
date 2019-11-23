6b81fd58c130e0f4fc582a752b9b1f0b9dede4fc
JackieW001 <jmw499@cornell.edu>
2019-11-23 16:24:15 -0500

    Serial2

--- MultiSerial.ino
+++ MultiSerial.ino
@@ -92,35 +92,53 @@ LobotServoController arm(Serial1);
 void setup() {
   Serial1.begin(9600);
   Serial.begin(9600);
-  while(!Serial1);
+  Serial2.begin(9600);
+// while(!Serial1);
 }
 
-void loop() {    
-  String pos_str = Serial1.readString();
-  // Parse the string into angles
-  int idx_begin = 0;
-  int idx_end = pos_str.length();
-  int idx_comma = pos_str.indexOf(',', idx_begin);
-  float angle6 = pos_str.substring(idx_begin, idx_comma).toFloat();
-  idx_begin = idx_comma+1;
-  idx_comma = pos_str.indexOf(',', idx_begin);
-  float angle5 = pos_str.substring(idx_begin, idx_comma).toFloat();
-  idx_begin = idx_comma+1;
-  idx_comma = pos_str.indexOf(',', idx_begin);
-  float angle4 = pos_str.substring(idx_begin, idx_comma).toFloat();
-  idx_begin = idx_comma+1;
-  idx_comma = pos_str.indexOf(',', idx_begin);
-  float angle3 = pos_str.substring(idx_begin, idx_comma).toFloat();
-  idx_begin = idx_comma+1;
-  float angle2 = pos_str.substring(idx_begin, idx_end).toFloat();
+float angle6; 
+float angle5;
+float angle4; 
+float angle3;
+float angle2; 
 
-  Serial.println(angle6);
-  Serial.println(angle5);
-  Serial.println(angle4);
-  Serial.println(angle3);
-  Serial.println(angle2);
+void loop() { 
+  //Serial.println(Serial1.readString());   
+  
+  // Parse the string into angles
+  if (Serial2) {
+    String pos_str = Serial2.readString();
+    int idx_begin = 0;
+    int idx_end = pos_str.length();
+    int idx_comma = pos_str.indexOf(',', idx_begin);
+    angle6 = pos_str.substring(idx_begin, idx_comma).toFloat();
+    idx_begin = idx_comma+1;
+    idx_comma = pos_str.indexOf(',', idx_begin);
+    angle5 = pos_str.substring(idx_begin, idx_comma).toFloat();
+    idx_begin = idx_comma+1;
+    idx_comma = pos_str.indexOf(',', idx_begin);
+    angle4 = pos_str.substring(idx_begin, idx_comma).toFloat();
+    idx_begin = idx_comma+1;
+    idx_comma = pos_str.indexOf(',', idx_begin);
+    angle3 = pos_str.substring(idx_begin, idx_comma).toFloat();
+    idx_begin = idx_comma+1;
+    angle2 = pos_str.substring(idx_begin, idx_end).toFloat();
+
+    Serial.println("inside loop");
+    Serial.println(angle6);
+    Serial.println(angle5);
+    Serial.println(angle4);
+    Serial.println(angle3);
+    Serial.println(angle2);
+  }
+  Serial.println("out of loop");
+//  Serial.println(angle6);
+//  Serial.println(angle5);
+//  Serial.println(angle4);
+//  Serial.println(angle3);
+//  Serial.println(angle2);
  
-  //arm.moveServos(5, 1000, 2, J2_deg_to_pos(angle2), 3, J3_deg_to_pos(angle3), 4, J4_deg_to_pos(angle4), 5, J5_deg_to_pos(angle5), 6, J6_deg_to_pos(angle6));
+  arm.moveServos(5, 1000, 2, J2_deg_to_pos(angle2), 3, J3_deg_to_pos(angle3), 4, J4_deg_to_pos(angle4), 5, J5_deg_to_pos(angle5), 6, J6_deg_to_pos(angle6));
   // arm.moveServos(4, 1000, 1, J1_deg_to_pos(90), 2, 1000, 3, 1000, 4, 1000);
   // delay(1000); 
   // arm.moveServos(4, 1000, 1, J1_deg_to_pos(0), 2, 1000, 3, 1000, 4, 1000);
@@ -130,7 +148,7 @@ void loop() {
   //delay(1000);
   //Serial.println(J1_deg_to_pos (10));
   //arm.moveServos(1,1500,2,1000);
-  //delay(1000);
+  delay(1000);
 
   
 }
