809fde9f0a27b0be5572715fcbf18bbc0c36aaf7
Dilan Lakhani <dilansmacbookair@dhcp-hol-244.redrover.cornell.edu>
2020-02-15 16:14:44 -0500

    Adding functions for stepper movement

--- MovingSteppers/MovingSteppers.ino
+++ MovingSteppers/MovingSteppers.ino
@@ -99,13 +99,13 @@ void moveJ4(int dir, double angle){
 
 void moveJ5(int dir,double angle){
   //moves the J5 stepper motor
-  //this motor has a gearRatio of 14, so 14:1 speed reducer 
+  //this motor has a gearRatio of 1, so 1:1 speed reducer 
   //microstepping 1 means 200 steps per revolution
   //microstepping 2 then 400 steps/rev, and so on
   //use microstep = 2 on this motor
-  int microStep = 2;
+  int microStep = 800;
   int StepsPerRev = 200;
-  int gearRatio = 19;
+  int gearRatio = 1;
   //21.1111 steps to move one degree, known from testing
   double stepsPerDegree = microStep*stepsPerRev/360; 
   double stepsToTurn = stepsPerDegree * angle;
