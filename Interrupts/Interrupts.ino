2b464020cb0c3940e9b34398365f9318919b33e9
Jacob Tamor <jwt95@cornell.edu>
2021-09-16 21:12:31 -0400

    serial send edits

--- Interrupts/Interrupts.ino
+++ Interrupts/Interrupts.ino
@@ -12,22 +12,22 @@
 
 // step pins 
 int s0 = 0;
-int s1 = 0;
+int s1 = 26;
 int s2 = 3;
 int s3 = 0;
 int s4 = 0;
 int s5 = 0;
 // direction pins
 int d0 = 0;
-int d1 = 0;
+int d1 = 27;
 int d2 = 2;
 int d3 = 0;
 int d4 = 0;
 int d5 = 0;
 //chip select pins
 int c0 = 0;
-int c1 = 8;
-int c2 = 9;
+int c1 = 9;
+int c2 = 0;
 int c3 = 0;
 int c4 = 0;
 int c5 = 0;
@@ -35,6 +35,7 @@ int c5 = 0;
 uint8_t send_buf[10];
 
 volatile int counter = 0;
+volatile int fill_serial_buffer = false;
 
 //Storing pins and states for each motor
 MovingSteppersLib motors[6] {{s0,d0,c0},{s1,d1,c1},{s2,d2,c2},{s3,d3,c3},{s4,d4,c4},{s5,d5,c5}};  //Instantiate Motors (StepPin, DirectionPin, EncoderChipSelectPin)
@@ -43,7 +44,7 @@ int directionPin[6] = {d0,d1,d2,d3,d4,d5};
 volatile int move [6]; //volatile because changed in ISR
 volatile int state [6]; //volatile because changed in ISR
 
-int reversed[6] = {0, 0, 0, 1, 1, 0}; // motors that have encoders facing the wrong way must pick direction changes slightly differently (opposite of normal)
+int reversed[6] = {0, 1, 0, 1, 1, 0}; // motors that have encoders facing the wrong way must pick direction changes slightly differently (opposite of normal)
 
 //Storing encoder values
 volatile float encoderDiff[6];  // units of encoder steps
@@ -63,6 +64,8 @@ void setup()
   send_buf[8] = 255;
   send_buf[9] = 253;
 
+  Serial1.flush();
+
 //  motors[0].encoder.setZeroSPI(c0); // zero the encoder at desired position
 //  motors[1].encoder.setZeroSPI(c1);
 //  motors[2].encoder.setZeroSPI(c2);
@@ -74,7 +77,7 @@ void setup()
 //   targetAngle[i] = 20;   // used for testing, this will be an input from object detection
 //   targetAngle[0] = 290;
 
-//   targetAngle[1] = 66;
+   targetAngle[1] = 40;
 //   targetAngle[2] = 45;
    
 //   targetAngle[1] = 100;
@@ -86,7 +89,7 @@ void setup()
   // targetAngle[1] = 80;
 //   targetAngle[2] = 170;  
    
-    targetAngle[3] = 300;
+  //  targetAngle[3] = 300;
    //targetAngle[4] = 100; 
     
     pinMode(directionPin[i], OUTPUT); //set direction and step pins as outputs
@@ -95,12 +98,12 @@ void setup()
    
     move[i] = 0; //default is to move none
    
-    move[0] = 1; //enable j1 // send move to the jetson and recieve the encoder directions from the jetson
+//    move[0] = 1; //enable j1 // send move to the jetson and recieve the encoder directions from the jetson
     move[1] = 1; // enable j2
-    move[2] = 1; // enable j3 
-    move[3] = 1; //enable j4
-    move[4] = 1; //enable j5
-    move[5] = 1; // enable j6
+//    move[2] = 1; // enable j3 
+//    move[3] = 1; //enable j4
+//    move[4] = 1; //enable j5
+//    move[5] = 1; // enable j6
    
     encoderTarget[i] = targetAngle[i] * 45.51111; //map degree to encoder steps
     encoderPos[i] = motors[i].encoder.getPositionSPI(14); //get starting encoder position
@@ -121,7 +124,7 @@ void setup()
 ISR(TIMER1_OVF_vect) //ISR to pulse pins of moving motors
 {
   TCNT1 = 65518;            // preload timer to 300 us
-
+  fill_serial_buffer = true;
   
   for (int i=0; i<6; i++){
 
@@ -169,7 +172,10 @@ void loop()
      checkDirLongWay(i); 
   }
 
-  makeSerBuffers();
+  if(fill_serial_buffer){
+     makeSerBuffers();
+  }
+ 
 //Serial.println(motors[1].encoder.getPositionSPI(14)); 
 //Serial.println(motors[1].encoder.getPositionSPI(14));
 }
@@ -194,14 +200,16 @@ void checkDirLongWay(int motorNum){ //checks that motor is moving in right direc
 }
 
 void makeSerBuffers(){
-
+  
+  fill_serial_buffer = false;
   for (int i=0; i<6; i++) {
 //    send_buf[2*i2+] = (motors[i].encoder.getPositionSPI(14) >> 8) & 255;  
 //    send_buf[2*i+3] =  motors[i].encoder.getPositionSPI(14) & 255; 
       send_buf[2+i] = move[i];
   }
   //delay(500);
-  Serial.println(send_buf[5]);
+  //Serial.println(send_buf[5]);
+  Serial1.flush();
   Serial1.write(send_buf, sizeof(send_buf));
 }
 
