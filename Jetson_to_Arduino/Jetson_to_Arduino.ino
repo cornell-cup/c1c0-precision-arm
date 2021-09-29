9f2f1e6bf61cbcf86581f05d2268e2e3c22b815c
Ariana Haghighi <ah677@cornell.edu>
2021-09-28 21:26:45 -0400

    Added Jetson to Arduino Communication File

--- Jetson_to_Arduino/Jetson_to_Arduino.ino
+++ Jetson_to_Arduino/Jetson_to_Arduino.ino
@@ -1,8 +1,13 @@
 // Jetson to Arduino Communication 
 #include <MovingSteppersLib.h>
 #include <MotorEncoderLib.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(19,18); // RX, TX
 
 #define MAX_ENCODER_VAL 16383
+
+int i = 0;
 
 /* PROBLEMS LIST
 
@@ -32,34 +37,30 @@ int c3 = 0;
 int c4 = 0;
 int c5 = 0;
 
-uint8_t receive_buf[10];
+// uint8_t receive_buf[10];
 
-//Storing pins and states for each motor
-MovingSteppersLib motors[6] {{s0,d0,c0},{s1,d1,c1},{s2,d2,c2},{s3,d3,c3},{s4,d4,c4},{s5,d5,c5}};  //Instantiate Motors (StepPin, DirectionPin, EncoderChipSelectPin)
-int stepPin[6] = {s0,s1,s2,s3,s4,s5}; 
-int directionPin[6] = {d0,d1,d2,d3,d4,d5};  
-volatile int move [6]; //volatile because changed in ISR
-volatile int state [6]; //volatile because changed in ISR
-
-int reversed[6] = {0, 1, 1, 1, 1, 0}; // motors that have encoders facing the wrong way must pick direction changes slightly differently (opposite of normal)
-
-//Storing encoder values
-volatile float encoderDiff[6];  // units of encoder steps
-volatile float encoderTarget[6];  // units of encoder steps
-volatile float targetAngle[6];   // units of degrees
-float encoderPos[6];   // units of encoder steps
-
-volatile int nottolerant; // motor not within expected position
+// read port 8 or 10 using serial 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600); //Baud Rate
+  Serial.begin(9600); // set Baud Rate
   Serial1.begin(9600); 
   Serial1.flush();
+  Serial.println("Hello World"); // Serial is what is print
+  //mySerial.begin(9600);
+  //mySerial.println("Bonjour"); 
+  Serial1.println("Bonjour"); 
+ 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (Serial1.available())
+    //Serial.println('\n');
+    Serial.write(Serial1.read());
+  // flush the serial every 100 iterations 
+  if ((i % 10) == 0) 
+    Serial1.flush();
+  
 }
