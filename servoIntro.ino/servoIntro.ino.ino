200d0b6f528d3b186930af33f232930637943b9a
Ariana Haghighi <ah677@cornell.edu>
2022-02-19 15:49:53 -0500

    continuous servo movement

--- servoIntro.ino/servoIntro.ino.ino
+++ servoIntro.ino/servoIntro.ino.ino
@@ -7,17 +7,25 @@ Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
 
 void setup() {
+  // servo library only supports pins 9 and 10 
+  Serial.begin(9600);
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.println(myservo.attached()); 
+   myservo.write(80);
+  delay(200);
+  myservo.write(90);
 }
 
 void loop() {
-  for (pos = 0; pos <= 30; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 30; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+// for continuous servo, write(n) sets the speed
+
+//  for (pos = 0; pos <= 30; pos += 1) { // goes from 0 degrees to 180 degrees
+//    // in steps of 1 degree
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(15);                       // waits 15ms for the servo to reach the position
+//  }
+//  for (pos = 30; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(15);                       // waits 15ms for the servo to reach the position
+//  }
 }
