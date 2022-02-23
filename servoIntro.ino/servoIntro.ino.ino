97a38c25172f2c2385ae98e40429238c8baef470
Ariana Haghighi <ah677@cornell.edu>
2022-02-22 19:57:27 -0500

    continuous servo

--- servoIntro.ino/servoIntro.ino.ino
+++ servoIntro.ino/servoIntro.ino.ino
@@ -1,6 +1,7 @@
 
 #include <Servo.h>
 
+// Futaba one isn't working? maybe because it is analog?
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
@@ -8,24 +9,37 @@ int pos = 0;    // variable to store the servo position
 
 void setup() {
   // servo library only supports pins 9 and 10 
+
+  // write(n) controls the speed (forward 0 to 89, backward 91 to 180)
   Serial.begin(9600);
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   Serial.println(myservo.attached()); 
-   myservo.write(80);
-  delay(200);
+
+  myservo.writeMicroseconds(1500);
+
+ // myservo.write(100);
+ // delay(2000);
+ // myservo.write(90);
+ // delay(2000);
+ // TODO: how to get the arm to move to calculate specific angles to go to
+ // minor issue: repeating the same angles moves the servo more and more in one direction over time
   myservo.write(90);
+
 }
 
 void loop() {
-// for continuous servo, write(n) sets the speed
-
-//  for (pos = 0; pos <= 30; pos += 1) { // goes from 0 degrees to 180 degrees
-//    // in steps of 1 degree
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//  }
-//  for (pos = 30; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//  }
+  // for continuous servo, write(n) sets the speed
+//  myservo.write(120);
+//  delay(500);
+//  myservo.write(90);
+//  delay(2000);
+//  myservo.write(60);
+//  delay(500); 
+//  myservo.write(90);
+//
+//  myservo.write(60);
+//  delay(500);
+//  myservo.write(120);
+//  delay(500);
+//  myservo.write(90);
 }
