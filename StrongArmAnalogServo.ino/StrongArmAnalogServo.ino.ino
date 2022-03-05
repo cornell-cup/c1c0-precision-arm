392a4e695160a691d1586a4ef424dfba76886722
Ariana Haghighi <ah677@cornell.edu>
2022-03-05 15:56:03 -0500

    anaglog servo

--- StrongArmAnalogServo.ino/StrongArmAnalogServo.ino.ino
+++ StrongArmAnalogServo.ino/StrongArmAnalogServo.ino.ino
@@ -1,14 +1,10 @@
 #include <Servo.h>
 
-// Futaba one isn't working? maybe because it is analog?
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
+Servo myservo;  
+int pos = 0;    
 
 void setup() {
   // servo library only supports pins 9 and 10 
-
   // write(n) controls the speed (forward 0 to 89, backward 91 to 180)
   Serial.begin(9600);
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
@@ -18,17 +14,17 @@ void setup() {
  // TODO: how to get the arm to move to calculate specific angles to go tos
  // minor issue: repeating the same angles moves the servo more and more in one direction over time
  // may need to add 'high precision resistors in order to rectify imprecise movements
-  myservo.write(90);
+  myservo.analogWrite(90);
 }
 
 void loop() {
   // for continuous servo, write(n) sets the speed
-  myservo.write(150);
+  myservo.analogWrite(150);
   delay(200);
-  myservo.write(90);
+  myservo.analogWrite(90);
   delay(2000);
-  myservo.write(50);
-  delay(200); 
-  myservo.write(90);
-  delay(2000);
+//  myservo.analogWrite(50);
+//  delay(200); 
+//  myservo.analogWrite(90);
+//  delay(2000);
 }
