12440afa27bc2f661ed0f9417501586e178d88d1
Liam Kain <kainliam0@gmail.com>
2023-09-09 13:20:06 -0400

    Shoulder test

--- ShoulderMotorTest/shouldertest/shouldertest.ino
+++ ShoulderMotorTest/shouldertest/shouldertest.ino
@@ -1,25 +1,58 @@
-int driverPUL = 13;
-int driverDIR = 12;
+#define driverPUL 10
+#define driverDIR 9
+#define EB 3
+#define EA 2
+int stepPerRev = 200;
+int pd = 1000 * 1000;
+int wait = 2;
+volatile bool setdir = LOW;
+int encoderCnt = 0;
+int ea_on = 0;
+int eb_on = 0;
 
-int pd = 500;
-boolean setdir = LOW;
-
-void revmotor() {
+void revmotor()
+{
   setdir = !setdir;
+  Serial.println("reversing");
 }
+void step()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    digitalWrite(driverDIR, setdir);
+    digitalWrite(driverPUL, HIGH);
+    delayMicroseconds(pd);
+    digitalWrite(driverPUL, LOW);
+    delayMicroseconds(wait * pd);
+  }
+}
+void ea()
+{
+  ea_on = 1;
 
-void setup() {
+}
+void eb()
+{
+  eb_on = 1;
+
+}
+void setup()
+{
+  Serial.begin(115200);
   // put your setup code here, to run once:
   pinMode(driverPUL, OUTPUT);
   pinMode(driverDIR, OUTPUT);
-  //attachInterrupt(digitalPinToInterrupt(reverseSwitch), revmotor, FALLING);
+  Serial.println("setup");
+  attachInterrupt(EA,ea,RISING);
+  digitalWrite(driverPUL, LOW);
+  digitalWrite(driverDIR, HIGH);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(driverDIR, setdir);
+void loop()
+{
+  digitalWrite(driverDIR, !setdir);
   digitalWrite(driverPUL, HIGH);
-  delayMicroseconds(pd);
+  delay(1);
   digitalWrite(driverPUL, LOW);
-  delayMicroseconds(pd);
+  delay(10);
 }
