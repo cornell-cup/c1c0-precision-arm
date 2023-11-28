ae3953b9e41e67b50ad81ac36de745371359fa4c
Liam Kain <kainliam0@gmail.com>
2023-11-27 21:59:10 -0500

    Blue Arm updated

--- blue_arm_main/blue_arm_main.ino
+++ blue_arm_main/blue_arm_main.ino
@@ -1,145 +1,304 @@
-#include <LobotServoController.h>
+// Interrupt Control for LeArm
 
-// J1: 1500 - 2500
-// J2: 500 - 2500
-// J3: 500 - 2000 (2500 on App)
-// J4: (max range depends on J5) 500 -2500
-// J5: 500 - 2500
-// J6: 500 - 2500
+// #include <MovingSteppersLib.h>
+// #include <MotorEncoderLib.h>
+#include "R2Protocol.h"
+#include "LobotServoController.h"
 
-/*************** Move Individual Motors *****************/
-int J1_deg_to_pos (float deg){
+// use interrupts file for jetson to arduino communicaiton
+
+#define MAX_ENCODER_VAL 16383
+
+/* PROBLEMS LIST
+  1. DO NOT HAVE TWO MOTORS HAVE SAME DIRECTION OR STEP PINS AS ANOTHER MOTOR EVERRRRR IT MESSES UP CODE
+*/
+
+// step pins 2
+int s0 = 48;
+int s1 = 35;
+int s2 = 0;
+int s3 = 0;
+int s4 = 0;
+int s5 = 0;
+// direction pins
+int d0 = 36;
+int d1 = 34;
+int d2 = 0;
+int d3 = 0;
+int d4 = 0;
+int d5 = 0;
+// chip select pins
+int c0 = 10;
+int c1 = 9;
+int c2 = 0;
+int c3 = 0;
+int c4 = 0;
+int c5 = 0;
+
+// Move Individual Motors
+int J1_deg_to_pos(float deg)
+{
   int lower = 1500;
-  int upper = 2500; 
-  int range = upper - lower; 
+  int upper = 2500;
+  int range = upper - lower;
   int pos = -1;
-  if (deg >= 0 && deg <= 180) {
-    pos = ((deg/180) * range) + lower;
+  if (deg >= 0 && deg <= 180)
+  {
+    pos = ((deg / 180) * range) + lower;
   }
   return int(pos);
 }
 
-int J2_deg_to_pos (float deg){
+int J2_deg_to_pos(float deg)
+{
   int lower = 500;
-  int upper = 2500; 
-  int range = upper - lower; 
+  int upper = 2500;
+  int range = upper - lower;
   int pos = -1;
-  if (deg >= 0 && deg <= 180) {
-    pos = ((deg/180) * range) + lower;
+  if (deg >= 0 && deg <= 180)
+  {
+    pos = ((deg / 180) * range) + lower;
   }
   return pos;
 }
 
-int J3_deg_to_pos (float deg){
+int J3_deg_to_pos(float deg)
+{
   int lower = 500;
-  int upper = 2000; 
-  int range = upper - lower; 
+  int upper = 2500;
+  int range = upper - lower;
   int pos = -1;
-  if (deg >= 0 && deg <= 180) {
-    pos = ((deg/180) * range) + lower;
+  if (deg >= 0 && deg <= 150)
+  {
+    pos = ((deg / 180) * range) + lower;
   }
   return pos;
 }
 
-int J4_deg_to_pos (float deg){
+int J4_deg_to_pos(float deg)
+{
   int lower = 500;
-  int upper = 2500; 
-  int range = upper - lower; 
+  int upper = 2500;
+  int range = upper - lower;
   int pos = -1;
-  if (deg >= 0 && deg <= 180) {
-    pos = ((deg/180) * range) + lower;
+  if (deg >= 0 && deg <= 180)
+  {
+    pos = ((deg / 180) * range) + lower;
   }
   return pos;
 }
 
-int J5_deg_to_pos (float deg){
+int J5_deg_to_pos(float deg)
+{
   int lower = 500;
-  int upper = 2500; 
-  int range = upper - lower; 
+  int upper = 2500;
+  int range = upper - lower;
   int pos = -1;
-  if (deg >= 0 && deg <= 180) {
-    pos = ((deg/180) * range) + lower;
+  if (deg >= 0 && deg <= 180)
+  {
+    pos = ((deg / 180) * range) + lower;
   }
   return pos;
 }
 
-int J6_deg_to_pos (float deg){
+int J6_deg_to_pos(float deg)
+{
   int lower = 500;
-  int upper = 2500; 
-  int range = upper - lower; 
+  int upper = 2500;
+  int range = upper - lower;
   int pos = -1;
-  if (deg >= 0 && deg <= 180) {
-    pos = ((deg/180) * range) + lower;
+  if (deg >= 0 && deg <= 180)
+  {
+    pos = ((deg / 180) * range) + lower;
   }
   return pos;
 }
-/*************** End Move Individual Motors *****************/
 
-int convert_deg_to_pos (int motor, float deg) {
-  switch (motor){
-    case 1: J1_deg_to_pos(deg);
-    case 2: J2_deg_to_pos(deg);
-    case 3: J3_deg_to_pos(deg);
-    case 4: J4_deg_to_pos(deg);
-    case 5: J5_deg_to_pos(deg);
-    case 6: J6_deg_to_pos(deg);
-    default: -1; 
-  }
+// SoftwareSerial mySerial(19,18); // RX, TX
+
+uint8_t send_buf[10];
+int i = 0;
+
+// Jetson to Arduino Set up
+uint8_t do_init = 1;
+uint16_t checksum;
+char type[5];
+uint8_t data[12];
+uint32_t data_len = 12;
+
+uint8_t receive_buf[256];
+uint8_t buf2[256]; // added, just for test
+
+volatile int counter = 0;
+volatile int fill_serial_buffer = false;
+void reset_input_buffer()
+{
+  while (Serial1.available() > 0)
+    Serial1.read(); // modified to serial 2
+  delay(100);
 }
 
-LobotServoController arm(Serial1);
+// Arduino to Jetson R2
+uint16_t servo_angles[] = {10, 20, 30, 40, 50, 60};
+// LobotServo servos[6];   //an array of struct LobotServo
+// servos[0].ID = 1;       //No.1 servo
+// servos[0].Position = 0;  //1400 position
+// servos[1].ID = 4;       //No.4 servo
+// servos[1].Position = 0;  //700 position
 
-void setup() {
-  Serial1.begin(9600);
-  Serial.begin(9600);
-  Serial2.begin(9600);
-// while(!Serial1);
-}
+uint16_t new_servo_angles[] = {0};
+uint8_t servo_anglesB8[12];
+uint8_t send_buffer[256];
 
-float angle6; 
-float angle5;
-float angle4; 
-float angle3;
-float angle2; 
+LobotServoController arm(Serial2);
 
-void loop() { 
-  //Serial.println(Serial1.readString());   
-  
-  // Parse the string into angles
-  if (Serial2) {
-    String pos_str = Serial2.readString();
-    int idx_begin = 0;
-    int idx_end = pos_str.length();
-    int idx_comma = pos_str.indexOf(',', idx_begin);
-    angle6 = pos_str.substring(idx_begin, idx_comma).toFloat();
-    idx_begin = idx_comma+1;
-    idx_comma = pos_str.indexOf(',', idx_begin);
-    angle5 = pos_str.substring(idx_begin, idx_comma).toFloat();
-    idx_begin = idx_comma+1;
-    idx_comma = pos_str.indexOf(',', idx_begin);
-    angle4 = pos_str.substring(idx_begin, idx_comma).toFloat();
-    idx_begin = idx_comma+1;
-    idx_comma = pos_str.indexOf(',', idx_begin);
-    angle3 = pos_str.substring(idx_begin, idx_comma).toFloat();
-    idx_begin = idx_comma+1;
-    angle2 = pos_str.substring(idx_begin, idx_end).toFloat();
-
-    Serial.println("inside loop");
-    Serial.println(angle6);
-    Serial.println(angle5);
-    Serial.println(angle4);
-    Serial.println(angle3);
-    Serial.println(angle2);
-  }
-  Serial.println("out of loop");
-//  Serial.println(angle6);
-//  Serial.println(angle5);
-//  Serial.println(angle4);
-//  Serial.println(angle3);
-//  Serial.println(angle2);
- 
-  arm.moveServos(5, 1000, 2, J2_deg_to_pos(angle2), 3, J3_deg_to_pos(angle3), 4, J4_deg_to_pos(angle4), 5, J5_deg_to_pos(angle5), 6, J6_deg_to_pos(angle6));
+void update_servo_angles(int k)
+{
+  arm.getPosition(k);
   delay(1000);
+  if (Serial2.available() > 0)
+  {
+    Serial.println("Bytes available: " + String(Serial2.available())); // 2 serials updated to serial 2
+    Serial2.readBytes(receive_buf, 8);                                 // serial update to 2
+    for (i = 0; i < 8; i++)
+    {
+      Serial.println(receive_buf[i]);
+    }
+    //      Serial.println("Data");
+    //      for (i=0; i<data_len; i++){
+    //        Serial.println(data[i]);
+    //      }
+    //     //Serial.println(data[1]);
+    //     convert_b8_to_b16(data,new_servo_angles);
+    //     changeAngles(new_servo_angles);
+  }
+  // Send read command to LeArm
+  // How to read value from the arm???
+  // servo_angles[k] = Serial2.read()
+}
 
-  
+void changeAngles(uint16_t data[])
+{
+  // TODO: Call moveServos function for all 6 new joint angles
+  // data[] will be 6 elements, call moveservos in each element
+  // Position in the array is the joint, value is the angle
+  memcpy(servo_angles,data,12);
+  Serial.println("New Angles:");
+  for (i = 0; i < 6; i++)
+  {
+    Serial.print(String(servo_angles[i]) + " ");
+  }
+  Serial.println();
+  arm.moveServos(6, 1000, 1, J1_deg_to_pos(data[0]), 2, J2_deg_to_pos(data[5]), 3, J3_deg_to_pos(data[4]), 4, J4_deg_to_pos(data[3]), 5, J5_deg_to_pos(data[2]), 6, J6_deg_to_pos(data[1]));
+  // delay(1000);
+}
+
+void setup()
+{
+  Serial.begin(115200); // Baud Rate
+  Serial1.begin(38400);
+  Serial2.begin(9600);
+
+  Serial.println("Hello World");
+  //  delay(1000);
+  // reset_input_buffer();
+}
+
+void send(char type[5], const uint8_t *data, uint32_t data_len, uint8_t *send_buffer)
+{
+  uint32_t written = r2p_encode(type, data, data_len, send_buffer, 256);
+  Serial1.write(send_buffer, written); // modified to serial 2
+  Serial1.flush();
+  // Serial.println("Bytes written: " + String(written));
+  //   for(int i=0; i <written; i++){
+  //     Serial.print(send_buffer[i],HEX);
+  //   }
+  //   Serial.println();
+}
+
+uint16_t init_data[] = {90,90,90,90, 90, 90};
+
+void loop()
+{
+  if (do_init)
+  {
+    changeAngles(init_data);
+    convert_b16_to_b8(init_data, servo_anglesB8, 6);
+    send("PRMR", servo_anglesB8, 12, send_buffer);
+    delay(1000);
+  }
+
+  // Jetson to Arduino
+
+  if (Serial1.available())
+  {
+    // Serial.println("Bytes available: " + String(Serial1.available())); //2 serials updated to serial 2
+    Serial1.readBytes(receive_buf, 28); // serial update to 2
+    // Serial.println(r2p_decode(receive_buf, 256, &checksum, type, data, &data_len));
+    if(r2p_decode(receive_buf, 256, &checksum, type, data, &data_len))
+    {
+      Serial.println("message received");
+      Serial.println(type);
+      if (!strcmp(type, "PRMR"))
+      {
+        Serial.println("current angles requested");
+        uint16_t new_data[6] = {};
+        new_data[0] = servo_angles[0];
+        new_data[1] = servo_angles[5];
+        new_data[2] = servo_angles[4];
+        new_data[3] = servo_angles[3];
+        new_data[4] = servo_angles[2];
+        new_data[5] = servo_angles[1];
+        convert_b16_to_b8(servo_angles, servo_anglesB8, 6);
+        send("prm", servo_anglesB8, 12, send_buffer);
+        delay(100);
+      }
+      else if (!strcmp(type, "PRM"))
+      {
+        Serial.println("angles commanded");
+
+        do_init = 0;
+
+       
+        // Serial.println(data[1]);
+        convert_b8_to_b16(data, new_servo_angles);
+        changeAngles(new_servo_angles);
+        Serial.println("Changed angles");
+      }
+    }
+  }
+  // Arduino to Jetson
+  else
+  {
+    // update_servo_angles();
+  }
+}
+
+void convert_b8_to_b16(uint8_t *databuffer, uint16_t *data)
+{
+  int data_idx;
+  for (int i = 0; i < 12; i++)
+  {
+    data_idx = i / 2;
+    if ((i & 1) == 0)
+    {
+      // even
+      data[data_idx] = databuffer[i] << 8;
+    }
+    else
+    {
+      // odd
+      data[data_idx] |= databuffer[i];
+    }
+  }
+}
+void convert_b16_to_b8(int *databuffer, uint8_t *data, int len)
+{
+  int data_idx1;
+  int data_idx2;
+  for (int i = 0; i < 2 * len; i += 2)
+  {
+    data[i] = (databuffer[i / 2] >> 8) & 255;
+    data[i + 1] = (databuffer[i / 2]) & 255;
+  }
 }
